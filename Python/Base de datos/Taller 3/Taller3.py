5567b36523ab222ae190d9bb10ddc3b72f46369c
Giacomo Baldessari <103346031+gbaldessari@users.noreply.github.com>
2023-11-14 19:19:02 -0300

    taller 3

--- Python/Base de datos/Taller 3/Taller3.py
+++ Python/Base de datos/Taller 3/Taller3.py
@@ -1,6 +1,19 @@
 import psycopg2
 
-def iniciar():
+def queryGet(query):
+    cur = conexion.cursor()
+    cur.execute(query)
+    resultados = cur.fetchall()
+    cur.close()
+    return resultados
+
+def querySet(query):
+    cur = conexion.cursor()
+    cur.execute(query)
+    conexion.commit()
+    cur.close()
+
+def iniciarConexion():
     parametros = {
         "host": "localhost",
         "port": "5432",
@@ -8,42 +21,78 @@ def iniciar():
         "password": "postgres",
         "database": "postgres"
     }
-    global coneccion
-    coneccion = psycopg2.connect(**parametros)
-    
-def querySelect(coneccion,queryy):
-    cur = coneccion.cursor()
-    cur.execute(queryy)
-    resultados = cur.fetchall()
-    cur.close()
-    return resultados
-
-def queryInsert(coneccion,queryy):
-    cur = coneccion.cursor()
-    cur.execute(queryy)
-    coneccion.commit()
-    cur.close()
+    global conexion
+    conexion = psycopg2.connect(**parametros)
 
 def login(resultados):
-    usuario = input("Ingrese usuario: ")
-    contrasena = input("Ingrese contraseña: ")
-    for i in resultados:
-        if usuario == i[0] and contrasena == i[1]:
-            return i
-    agregar = input("Desea registrar un nuevo cliente (Y/N): ")
-    if(agregar == "Y"):
-        nombre = input("Ingrese el nombre del cliente: ")
-        numero = input("Ingrese el numero de telefono del cliente: ")
-        queryInsert(coneccion,"insert into cliente (correo,nombre,contrasena,numero_telefono) values ('" +usuario+"','"+nombre+"','"+contrasena+"','"+numero+"');")
+    while True:
+        usuario = input("Ingrese usuario: ")
+        contrasena = input("Ingrese contraseña: ")
+        for i in resultados:
+            if usuario == i[1] and contrasena == i[2]:
+                return i
+        valorRegistro = registro(usuario,contrasena)
+        if valorRegistro != False:
+            return valorRegistro
+        
+def registro(usuario,contrasena):
+    while True:
+        agregar = input("Desea registrar un nuevo cliente (Y/N): ").lower()
+        if agregar == "y":
+            nombre = input("Ingrese el nombre del cliente: ")
+            numero = input("Ingrese el numero de telefono del cliente: ")
+            querySet("insert into cliente (correo,nombre,contrasena,numero_telefono) values ('" +usuario+"','"+nombre+"','"+contrasena+"','"+numero+"');")
+            return queryGet("select nombre,correo,contrasena,numero_telefono,'cliente' from cliente where correo = '"+usuario+"'")[0]
+        elif agregar == "n":
+            return False
+        else:
+            print("Ingrese un valor valido")
 
+def registrarProducto():
+    id = input("Ingrese el id del producto: ")
+    nombre = input("Ingrese el nombre del producto: ")
+    precio = input("Ingrese el precio del producto: ")
+    descripcion = input("Ingrese la descripcion del producto: ")
+    stock = input("Ingrese el stock del producto: ")
+    querySet("insert into producto(id_producto,nombre,precio,descripcion,stock) values ('"+id+"','"+nombre+"','"+precio+"','"+descripcion+"','"+stock+"');")
 
-iniciar()
-resultados = querySelect(coneccion,"select correo,contrasena,'admin' from administrador union select correo,contrasena,'cliente' from cliente")
-usuario = login(resultados)
+def menuAdmin():
+    while True:
+        print("==Menu Administrador==")
+        print("1) Registrar nuevo producto")
+        print("2) Informacion de producto")
+        print("3) Actualizar inventario")
+        print("4) Productos bajos de stock")
+        print("5) Registrar venta")
+        print("6) Historial de ventas")
+        print("7) Salir")
+        opcion = input()
+        if opcion == "1":
+            registrarProducto()
+        elif opcion == "2":
+            print()
+        elif opcion == "3":
+            print()
+        elif opcion == "4":
+            print()
+        elif opcion == "5":
+            print()
+        elif opcion == "6":
+            print()
+        elif opcion == "7":
+            break
+        else:
+            print("Ingrese una opcion valida")
+        
 
-if(usuario[2] == "admin"):
-    # Menu admin
-    print("admin")
-elif(usuario[2]=="cliente"):
-    #Menu cliente
-    print("cliente")
+def menuCliente(usuarioIniciado):
+    print("Menu Cliente")
+
+iniciarConexion()
+resultados = queryGet("select nombre,correo,contrasena,' ','admin' from administrador union select nombre,correo,contrasena,numero_telefono,'cliente' from cliente")
+usuarioIniciado = login(resultados)
+
+if(usuarioIniciado[4] == "admin"):
+    menuAdmin()
+elif(usuarioIniciado[4]=="cliente"):
+    menuCliente(usuarioIniciado)
--- Sql/Taller 3/Taller3.sql
+++ Sql/Taller 3/Taller3.sql
@@ -1,38 +1,44 @@
-create table cliente (
+drop table if exists Cliente cascade;
+drop table if exists Administrador cascade;
+drop table if exists Compra cascade;
+drop table if exists Producto cascade;
+drop table if exists Detalle_compra cascade;
+
+create table Cliente (
     correo text primary key,
     nombre text not null,
     contrasena text not null,
     numero_telefono text not null
 );
-create table administrador (
+create table Administrador (
     correo text primary key,
     nombre text not null,
     contrasena text not null
 );
-create table compra (
+create table Compra (
     id_compra text primary key,
-    id_administrador text references administrador(correo) not null,
-    id_cliente text references cliente(correo) not null,
+    id_administrador text references Administrador(correo) not null,
+    id_cliente text references Cliente(correo) not null,
     fecha date not null
 );
-create table producto (
+create table Producto (
     id_producto text primary key,
     nombre text not null,
     precio int not null,
     descripcion text not null,
     stock int not null
 );
-create table detalle_compra (
-    id_compra text REFERENCES compra(id_compra),
-    id_producto text REFERENCES producto(id_producto),
+create table Detalle_compra (
+    id_compra text references Compra(id_compra),
+    id_producto text references Producto(id_producto),
     primary key (id_compra, id_producto),
     cantidad int not null
 );
 
-insert into administrador(correo,nombre,contrasena) values 
+insert into Administrador(correo,nombre,contrasena) values 
     ('laura@tienda.com','laura','LauR1234');
 
-insert into cliente(correo,nombre,contrasena,numero_telefono) values
+insert into Cliente(correo,nombre,contrasena,numero_telefono) values
     ('giacomo@baldessari.it','giacomo','odioItalia','666');
 
 select correo,contrasena,'admin' from administrador
