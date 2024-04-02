a2adb8d7a47b328486635127522551d52d817379
Giacomo Baldessari <103346031+gbaldessari@users.noreply.github.com>
2024-04-02 18:49:45 -0300

    Update MayorTresNumeros.asm

--- Assembly/Simuproc/Taller 1/MayorTresNumeros.asm
+++ Assembly/Simuproc/Taller 1/MayorTresNumeros.asm
@@ -1,30 +1,30 @@
 MSG ===Menu===
 MSG Ingrese el primer numero:
-LDT
-STA 02A
+LDT ;Ingreso el primer numero a ordenar
+STA 02A ;Guardo en 02A
 MSG Ingrese el segundo numero:
-LDT
-STA 02B
+LDT ;Ingreso el segundo numero a ordenar
+STA 02B ;Guardo en 02B
 MSG Ingrese el tercer numero:
-LDT
-STA 02C
+LDT ;Ingreso el tercer numero a ordenar
+STA 02C ;Guardo en 02C
 
-LDA 02A
-CMP 02B
-JMA 030
-STA 02A
+LDA 02A ;Muevo 02A a AX
+CMP 02B ;Comparo AX con 02B
+JMA 030 ;Si AX es mayor salta a 030
+STA 02A ;Guardo AX en 02A
 
 LDA 02B
 CMP 02C
 JMA 036
 STA 02B
 
+MSG Numeros ordenados:
+CLN
 LDA 02A
 EAP
-STA 02A
 LDA 02B
 EAP
-STA 02B
 LDA 02C
 EAP
 STA 02C
@@ -36,7 +36,7 @@ LDA 02B
 STA 02A
 LDA 02D
 STA 02B
-JMP 00A
+JMP 00E
 
 STA 02D
 LDA 02C
