fc843985ebd4dd87ceaf40027ae4f612fdbc6931
Giacomo Baldessari <103346031+gbaldessari@users.noreply.github.com>
2024-09-05 16:43:02 -0400

    Update clase_1.rkt

--- Scheme/Racket/clase_1.rkt
+++ Scheme/Racket/clase_1.rkt
@@ -2,6 +2,11 @@
 
 (displayln "Hello, world!")
 
+(displayln "h")
+(displayln "o")
+(displayln "l")
+(displayln "a")
+
 (define f (
   lambda (x) 
     (+ x 1)
@@ -28,11 +33,6 @@
 (displayln (sqrt 18))
 (displayln (sqrt (/ -1 4)))
 
-(displayln "h")
-(displayln "o")
-(displayln "l")
-(displayln "a")
-
 (define l (list 1 2 3 4 5 6))
 (displayln l)
 (car l)
@@ -42,7 +42,36 @@
 (cdr (cdr l))
 (displayln (length l))
 
+(car(cdr (list 1 2 3 4)))
+(cadr (list 1 2 3 4))
+
 (if (=(remainder (length l) 2) 0)
   (displayln #t)
   (displayln #f)
 )
+
+;(5 * (12/3 + 8/4) + 10/(2 + 3))
+(displayln (+ (* 5 (+ (/ 12 3) (/ 8 4))) (/ 10 (+ 2 3))))
+
+(define condicion 5)
+(cond
+  [(> condicion 5) (displayln "mayor a 5")]
+  [(< condicion 5) (displayln "menor a 5")]
+  [(= condicion 5) (displayln "igual a 5")]
+)
+
+(define for (
+  lambda (x) (
+    if (null? x) (
+      displayln "funcion terminada")
+      ;else
+      (begin
+        (displayln (car x))
+        (for (cdr x))
+      )
+    )
+  )
+)
+
+(define myList (list 1 2 3 4 5 6))
+(for myList)
